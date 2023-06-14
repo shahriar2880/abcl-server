0e302592892fdfff55b0368e726e9ee5ee0c6e31
shahriar2880 <shahriarreza436@mail.com>
2023-06-15 03:19:51 +0600

    com

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import bodyParser from "body-parser";
 import cors from "cors"
+import path from 'path'
+import { fileURLToPath } from "url";
+
 
 const app = express()
 
@@ -20,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //configure env
 dotenv.config();
+
 
 
 //routes
@@ -41,9 +45,20 @@ const PORT = process.env.PORT || 8000;
 //database config
 connectDB();
 
+//es module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename)
+
 //middleware
 app.use(morgan("dev"))
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'../abcl-client/build')))
+
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname,'../abcl-client/build/index.html'))
+})
+
 
 // Middleware for parsing URL-encoded bodies
 app.use(express.urlencoded({ extended: false }));
